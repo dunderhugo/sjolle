253710c9d34f9f885237a77b438632ec2c18bd09
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-03-26 16:22:13 +0100

    Fetch and display soduko API

---
+++ client/src/components/Sudoku/Sudoku.jsx
@@ -0,0 +1,51 @@
+import styles from './Sudoku.module.css';
+import { useEffect, useState,  } from 'react';
+
+function Sudoku() {
+    const [puzzle, setPuzzle] = useState([]);
+    const [solution, setSolution] = useState([]);
+    const [difficulty, setDifficulty] = useState("");
+
+
+    const fetchApi = async () => {
+        try{
+            const response = await fetch('https://sudoku-api.vercel.app/api/dosuku');
+            const data = await response.json();
+            console.log(data);
+            setPuzzle(data.newboard.grids[0].value);
+            setSolution(data.newboard.grids[0].solution);
+            setDifficulty(data.newboard.grids[0].difficulty);
+        }
+        catch{
+            console.log("error");
+        }
+    } 
+
+    useEffect(() => {
+        fetchApi();
+    }, []);
+
+
+    
+    return (
+        <div className={styles.container}>
+            <div className={styles.gameBoard}>
+                <h1>Sudoku</h1>
+                <h2>Difficulty: {difficulty}</h2>
+                <div className={styles.puzzle}>
+                    {puzzle.map((row, index) => (
+                        row.map((num, index) => (
+                            <div key={index} className={styles.cell}>
+                                {num === 0 ? " " : num}
+                            </div>
+                        ))
+                    ))}
+                </div>
+                <button onClick={fetchApi}>New Puzzle</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Sudoku;
