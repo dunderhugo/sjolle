e56bb5c014c65fa0b01fb7692d5f98ca21fc657f
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-06-22 00:04:43 +0200

    Worlde page implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,16 @@ import UnderConstruction from './pages/UnderConstruction/UnderConstruction';
 import Footer from './components/Footer/Footer'
 import Sudoku from './pages/Sudoku/Sudoku';
 import HomePage from './pages/HomePage/HomePage'
+import Wordle from './pages/Wordle/Wordle';
 
 function App() {
-
-
   return (
     <>
     {/* <Sudoku /> */}
-    <HomePage/>
+    {/* <HomePage/> */}
     {/* <UnderConstruction /> */}
     {/* <Footer /> */}
+    <Wordle/>
 
     </>
   )
