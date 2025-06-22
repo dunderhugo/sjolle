d0a45ff5d27b1702fd11cf2f7a5af4748a90ea24
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-06-22 16:13:03 +0200

    level selector shell for Sudoku

---
+++ docs/src/pages/Sudoku/LevelSelect.jsx
@@ -0,0 +1,19 @@
+import styles from './Sudoku.module.css';
+
+
+function LevelSelect(){
+
+
+
+
+    return(
+        <>
+        <h2>Select level</h2>
+        <h3>Easy</h3>
+        <h3>Medium</h3>
+        <h3>Hard</h3>
+        </>
+    );
+}
+
+export default LevelSelect;
