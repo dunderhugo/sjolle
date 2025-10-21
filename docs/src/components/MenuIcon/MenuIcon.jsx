07e445087848ebb32bce7554ff35aa79f97e358f
Hugo Forssell <147729983+dunderhugo@users.noreply.github.com>
2025-10-21 17:02:29 +0200

    Added component for icons

---
+++ docs/src/components/MenuIcon/MenuIcon.jsx
@@ -0,0 +1,11 @@
+import styles from "./MenuIcon.module.css"
+
+function MenuIcon(){
+  return(
+    <>
+    <h1 className={styles.title}>{gamesName}</h1>
+    </>
+  )
+}
+
+export default MenuIcon;
